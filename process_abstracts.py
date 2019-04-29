adb4d56e4970cd19570eea09f227b8d7d6cc3ed5
wilzzw <wilson@LAPTOP-CRSD13UA.localdomain>
2019-04-28 20:57:09 -0400

    Added comments

--- process_abstracts.py
+++ process_abstracts.py
@@ -1,38 +1,65 @@
+##################### Created by Wilson Zeng on April 27th, 2019 ##############################
+
 from bs4 import BeautifulSoup
 import subprocess
 import re
 import os
 
+##################### Mutable Inputs: Can & Should be Updated as it Goes ######################
+
+# Available image extensions (mutable)
+# This code will find image files associated with the following extensions in the current
+# directory by matching the image file name against Lastname_Firstname.docx
+# !Important: Potential manual work before executing the script is fixing typos in people's
+# filenames. Apparently people misspell their own names nowadays.
 image_ext = ['.png', '.jpg']
 
+# Extract file names in the current working directory into tuple: (file_name, extension)
 all_files = [os.path.splitext(f) for f in os.listdir('.') if os.path.isfile(f)]
 doc_files = [file_tuple for file_tuple in all_files if '.docx' in file_tuple[1]]
 fig_files = [file_tuple for file_tuple in all_files if file_tuple[1] in image_ext]
 
-#Ensure sorted by first author's last name
+# Sorted by submitters' last names alphabetically
 doc_files = sorted(doc_files, key=lambda x: x[0])
 
+# Code output will be written to output_tex
 output_tex = 'output.tex'
-#Clear output file
+
+# Clear current output file if available
 latex = open(output_tex, 'w')
 latex.close()
 
-#Sanity Zone; pllease update as you go..
+# Sanity Zone; please update as you go..
+# As far as I am aware, PANDOC processes '&' and '%' correctly.
+# Do not double-process them by adding to the dictionary special_characters!!
 greek = {'α': '$/alpha$', 'β': '$/beta$', 'γ': '$/gamma$', 'δ': '$/delta$',
          'ε': '$/epsilon$', 'ζ': '$/zeta$', 'η': '$/eta$', 'θ': '$/theta$',
          'ι': '$/iota$', 'κ': '$/kappa$', 'λ': '$/lambda$', 'μ': '$/mu$',
          'ν': '$/nu$', 'ξ': '$/xi$', 'π': '$/pi$', 'ρ': '$/rho$', 'σ': '$/sigma$',
          'τ': '$/tau$', 'υ': '$/upsilon$', 'φ': '$/phi$', 'χ': '$/chi$', 'ψ': '$/psi$', 'ω': '$/omega$'}
 latin_with_accents = {'é': r'\'{e}', 'è': r'\`{e}', 'ü': r'\"{u}', 'ä': r'\"{a}', 'ö': r'\"{o}'}
-fix_pandoc = {r'\textasciitilde{}': '$/sim$'}
-special_characters = {} #Pandoc handles most special characters already, except "~", which is addressed in fix_pandoc
 
-minor_fixes = {**greek, **latin_with_accents, **fix_pandoc, **special_characters}
+# The symbol I know PANDOC does not handle properly
+fix_pandoc = {r'\textasciitilde{}': '$/sim$' # PANDOC gives '\textasciitilde{}' for tilde, bit it ends up being the tilde that is too high up
+             }
+# PANDOC handles most special characters already, except "~", which is addressed in fix_pandoc (2019-04-28)
+# If there are any other special characters PANDOC didn't parse, might want to add it to special_characters.
+# For the ones which PANDOC process into something not desired, might want to add it to fix_pandoc instead.
+special_characters = {}
 
-#Name segments that should not be capitalized
+# Pool all fixes together. These fixes will be applied after we have allowed PANDOC to do its job (i.e. To clean up what PANDOC did not do a great job in).
+minor_fixes = {**greek, **latin_with_accents, **fix_pandoc, **special_characters} 
+
+# Name segments that should not be capitalized
+# Anything else? Please update :)
 do_not_capitalize = ['van', 'der', 'van\'t']
 
-##################### Functions #####################
+################################## Functions! Functions! ######################################
+# General conversion of text extracted from html into latex format
+# First let PANDOC handles the dirty work
+# Then apply our specific fixes
+# text: str, the text to be converted
+# Returns: str, the input text in latex format
 def textfix(text):
     #return text
     if '\n' in text:
@@ -51,6 +78,7 @@ def textfix(text):
     total_fixed_text = r'\\'.join(fixed_paragraphs)
     return total_fixed_text
 
+# Function to be called to write the beginning & end of the main abstract latex environment
 def choose_template(references=False, figure=False):
     if not references or figure:
         return {'begin': r'\begin{posterabs}', 'end': r'\end{posterabs}'}
@@ -60,9 +88,14 @@ def choose_template(references=False, figure=False):
         return {'begin': r'\begin{posterabswref}', 'end': r'\end{posterabswref}'}
     return {'begin': r'\begin{posterabswrefwfig}', 'end': r'\end{posterabswrefwfig}'}
 
+# Function to be called to write abstract tile in latex
 def proc_title(title=''):
     return '{'+title+'}'
 
+# Function to be called to process an author's information (variable 'author')
+# An author information is a tuple: (presenting, First_name, ..., Last_name)
+# If the author is the presenting author, presenting == True, and vice versa
+# Returns the name string of the author separated by spaces, as well as whether the author is presenting
 def proc_author_info(author):
     presenting = author[0]
     name_components = []
@@ -75,6 +108,9 @@ def proc_author_info(author):
         return {'name': r'\underline{'+' '.join(name_components)+'}', 'presenting?': presenting}
     return {'name': ' '.join(name_components), 'presenting?': presenting}
 
+# Function to be called to process a list of authors' information and the corresponding affiliation numbers
+# authors are a list of author information tuples; affiliations are a list of lists of corresponding affiliation numbers (as strings of integers)
+# Returns: 1. str, the list of authors with annotated affiliations in latex format; 2. presenting_author's label string in the format: LastF
 def proc_authors(authors, affiliations):
     author_tex = []
     label = ''
@@ -86,6 +122,7 @@ def proc_authors(authors, affiliations):
     author_tex_string = '{'+' '.join(author_tex)+'}'
     return {'tex': author_tex_string, 'label': label}
 
+# Function to be called to produce affiliations in latex format
 def proc_affil(affil_info):
     affiliation_texts = ['{']
     for n in sorted(list(affil_info.keys())):
@@ -96,15 +133,19 @@ def proc_affil(affil_info):
     affiliation_texts.append('}')
     return affiliation_texts
 
+# Function to be called to write poster number in latex format
 def proc_poster_num(poster_number='#'):
     return r'{P\#}'
 
+# Function to be called to write lines indicating inserting figure in latex format
+# width and height are in number of pixels
 def proc_fig(fig_file, width, height):
     fig_texts = ['{'+fig_file+'}']
     fig_texts.append('{'+str(width/100)+'}')
     fig_texts.append('{'+str(height/100)+'}')
     return fig_texts
 
+# Function to be called to produce references in latex format
 def proc_ref(ref_info):
     reference_texts = ['{']
     for n in sorted(list(ref_info.keys())):
@@ -115,6 +156,7 @@ def proc_ref(ref_info):
     reference_texts.append('}')
     return reference_texts
 
+# Function to be called to write the latex file output
 def write_latex(output_name, title, authors, associated_affiliations, affiliations_list, abstract_text, figure='', width=0, height=0, references=[]):
     if output_name[-4:] != '.tex' and '.' not in output_name:
         output_name += '.tex'
@@ -141,7 +183,7 @@ def write_latex(output_name, title, authors, associated_affiliations, affiliatio
     
     presenter_label = proc_authors(authors, associated_affiliations)['label']
 
-    #Some people forget to indicate presenting author wtf...
+    #Some people forget to indicate presenting author...
     if len(presenter_label) == 0:
         #Assume filename is the presenter
         file_id[0].split('_')
@@ -151,7 +193,7 @@ def write_latex(output_name, title, authors, associated_affiliations, affiliatio
     write_line(choose_template(ref_avail, fig_avail)['end'])
 
     presenting_author = [author[1:] for author in authors if author[0]]
-    #Some people forget to indicate presenting author wtf...
+    #Some people forget to indicate presenting author...
     if len(presenting_author) == 0:
         presenting_author = [tuple(file_id[0].split('_'))]
 
@@ -170,7 +212,7 @@ def write_latex(output_name, title, authors, associated_affiliations, affiliatio
     latex.close()
     return
 
-#docx to html by Pandoc. Below commands should be run under Linux/Unix Shell.
+# docx to html by PANDOC. The way the subprocess.run() is written in this code requires this python code to be run under Unix Shell, such as Bash.
 for file_id in doc_files:
     ##################### Part I. Parsing Word File into HTML #####################
     input_doc = ''.join(file_id)
@@ -178,7 +220,7 @@ for file_id in doc_files:
     html_name = file_id[0]+'.html'
     subprocess.run(['pandoc', input_doc, '-f', 'docx', '-t', 'html', '-o', html_name])
 
-    #Look whether submitter submitted a figure file
+    # Look whether submitter submitted a figure file
     matching_figures = [''.join(fig_id) for fig_id in fig_files if fig_id[0].lower() == file_id[0].lower()]
     if len(matching_figures) > 0:
         fig_name = matching_figures[0]
@@ -187,32 +229,32 @@ for file_id in doc_files:
     if len(fig_name) > 0:
         print('Submitter has a figure: '+fig_name+'\n')
 
-    #Parse html file into BeautifulSoup :) for ease of extracting contents
-    #See https://www.crummy.com/software/BeautifulSoup/bs4/doc/ for Beautiful Soup docs :)
+    # Parse html file into BeautifulSoup :) for ease of extracting contents
+    # See https://www.crummy.com/software/BeautifulSoup/bs4/doc/ for Beautiful Soup docs :)
     f = open(html_name, 'r')
     soup = BeautifulSoup(f, 'html.parser')
 
-    #Extract all table/cell contents into a list of table objects
+    # Extract all table/cell contents into a list of table objects
     all_tables = soup.find_all('table')
 
-    #If someone messed up
+    # If someone messed up the document so badly/did not use the submission template
     if len(all_tables) == 0:
-        print(input_doc+' is problematic. No tables were found. Please check the docx file manually as the submission format is likely wrong.\n')
+        print(input_doc+' is problematic. No tables were found. Please check the docx file manually as the submitter likely did not use the provided abstract template.\n')
         continue
 
-    #The first table has the abstract title (supposedly..)
+    # The first table has the abstract title (supposedly..)
     abstract_title_components = [textfix(str(c)) for c in all_tables[0].find_all(['th', 'td'])[0].contents]
     abstract_title = ''.join(abstract_title_components) ###Processed abstract title in latex format!
 
-    #The second table has the the authors & affiliations (supposedly..)
-    #Extract all fields ('tr') in the authors & affiliations table
+    # The second table has the the authors & affiliations (supposedly..)
+    # Extract all fields ('tr') in the authors & affiliations table
     author_and_affil_fields = all_tables[1].find_all('tr')
     author_list = []
     associated_affil = []
     for entry in author_and_affil_fields[1:]:
         num_author = int(re.sub('[^0-9]', '', entry.find_all('td')[0].text))
         author_name = entry.find_all('td')[1].text
-        #If no author in this line, skip over
+        # If no author in this line, skip over
         if len(author_name) == 0:
             continue
         author_name = author_name.split(' ')
@@ -221,10 +263,10 @@ for file_id in doc_files:
         author_info = tuple([presenting] + author_name)
         author_list.append(author_info)
         associated_affil.append(author_affil)
-    #The format of the author_list and associated_affil: author informations are tuples: tuple(presenting?, from first name to last name); affiliations are lists of corresponding affiliation numbers as strings (not integers).
+    # The format of the author_list and associated_affil: author informations are tuples: tuple(presenting?, from first name to last name); affiliations are lists of corresponding affiliation numbers as strings (not integers).
 
-    #The third table has the affiliation names
-    #Extract all fields ('tr') in the affiliations
+    # The third table has the affiliation names
+    # Extract all fields ('tr') in the affiliations
     affiliation_fields = all_tables[2].find_all('tr')
     affil_dict = {}
     for entry in affiliation_fields:
@@ -237,14 +279,14 @@ for file_id in doc_files:
         if len(affil) > 0:
             affil_dict[affil_num] = affil
 
-    #The fourth table has the abstract body text
+    # The fourth table has the abstract body text
     abstract_content = [textfix(str(c)) for c in all_tables[3].find_all(['th', 'td'])[0].contents]
     abstract_main = ''.join(abstract_content) ###Processed abstract text in latex format!
 
-    #The fifth table has the references
-    #Extract all fields ('tr') in the references
+    # The fifth table has the references
+    # Extract all fields ('tr') in the references
 
-    if len(all_tables) > 4: #People who delete their tables: I hate you. You are annoying!
+    if len(all_tables) > 4: #People who delete tables in the abstract template: I hate you. You are annoying!
         reference_fields = all_tables[4].find_all('tr')
         ref_list = {}
         for entry in reference_fields:
@@ -262,6 +304,7 @@ for file_id in doc_files:
     write_latex(output_tex, abstract_title, author_list, associated_affil, affil_dict, abstract_main, figure=fig_name, width=0, height=0, references=ref_list)
     print('\n')
 
+# Remove temporary html files
 html_files = [f for f in os.listdir('.') if os.path.isfile(f) and os.path.splitext(f)[1] == '.html']
 for htm in html_files:
     os.remove(htm)
